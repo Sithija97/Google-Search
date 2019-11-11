7184126aa524be7fa3958beff24abf5f5b7f2a77
unknown <nsithijashehara@gmail.com>
2019-11-11 15:43:23 +0530

    initilized firebase

--- components/Open.js
+++ components/Open.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//importing firebase
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDI79xZHw333zRe8P_3vhl8zVIakj4KvR8",
+    authDomain: "project-sn-76a96.firebaseapp.com",
+    databaseURL: "https://project-sn-76a96.firebaseio.com",
+    projectId: "project-sn-76a96",
+    storageBucket: "project-sn-76a96.appspot.com",
+  
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 export default class Open extends React.Component {
     render() {
         return (
