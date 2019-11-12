767ec38545312cf0a8a882b77a9166938d35b898
unknown <nsithijashehara@gmail.com>
2019-11-13 05:17:27 +0530

    implemented

--- components/Open.js
+++ components/Open.js
@@ -1,55 +1,76 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Button, Text, Content, Form, Input, Label, Header, Item } from 'native-base';
+import React, {Component} from 'react';
 
-import * as firebase from "firebase";
+import {StyleSheet} from 'react-native';
+
+import {
+  Container,
+  Button,
+  Text,
+  Content,
+  Form,
+  Input,
+  Label,
+  Header,
+  Item,
+} from 'native-base';
+
+import * as firebase from 'firebase';
 
 // // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC09rNDxUbMRYkTnMR5C9s0YKleq--sxvA",
-  authDomain: "mobile-dev-b16eb.firebaseapp.com",
-  databaseURL: "https://mobile-dev-b16eb.firebaseio.com",
-  projectId: "mobile-dev-b16eb",
-  storageBucket: "mobile-dev-b16eb.appspot.com",
-  messagingSenderId: "759211989822",
+  apiKey: 'AIzaSyC09rNDxUbMRYkTnMR5C9s0YKleq--sxvA',
+
+  authDomain: 'mobile-dev-b16eb.firebaseapp.com',
+
+  databaseURL: 'https://mobile-dev-b16eb.firebaseio.com',
+
+  projectId: 'mobile-dev-b16eb',
+
+  storageBucket: 'mobile-dev-b16eb.appspot.com',
+
+  messagingSenderId: '759211989822',
 };
+
 // // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
 
 export default class Open extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      email: "",
-      password: ""
+      email: '',
+
+      password: '',
     };
   }
 
-  async _loadResourcesAsync() {
-    const loadedFonts = await Font.loadAsync({
-      Roboto: require('./node_modules/native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
-    });
-    return Promise.all([loadedFonts]);
-  }
-  
   SignUpUser = (email, password) => {
     try {
       if (this.state.password.length < 6) {
-        alert("Please enter atleast 6 characters")
+        alert('Please enter atleast 6 characters');
+
         return;
       }
 
-      firebase.auth().createUserWithEmailAndPassword(email, password)
+      firebase.auth().createUserWithEmailAndPassword(email.trim(), password);
+    } catch (error) {
+      alert('invalid entry');
     }
-    catch (error) {
-      alert("invalid entry")
-    }
-  }
+  };
 
   LoginUser = (email, password) => {
-
-  }
+    try{
+      firebase.auth().signInWithEmailAndPassword(email.trim(),password).then(function(user){
+        console.log (user)
+      })
+    }
+    catch(error){
+      alert('invalid login');
+    }
+  };
 
   render() {
     return (
@@ -57,36 +78,43 @@ export default class Open extends Component {
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
+
             <Input
               autoCorrect={false}
               autoCapitalize="none"
-              onChangeText={(email) => this.setState({ email })}
-            />
-          </Item>
-          <Item floatingLabel>
-            <Label>Password</Label>
-            <Input
-              autoCorrect={false}
-              autoCapitalize="none"
-              onChangeText={(password) => this.setState({ password })}
+              onChangeText={email => this.setState({email})}
             />
           </Item>
 
-          <Button style={{ marginTop: 20 }}
+          <Item floatingLabel>
+            <Label>Password</Label>
+
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={password => this.setState({password})}
+            />
+          </Item>
+
+          <Button
+            style={{marginTop: 20}}
             full
             rounded
             success
-            onPress={() => this.LoginUser(this.state.email, this.state.password)}
-          >
+            onPress={() =>
+              this.LoginUser(this.state.email, this.state.password)
+            }>
             <Text>Login</Text>
           </Button>
 
-          <Button style={{ marginTop: 20 }}
+          <Button
+            style={{marginTop: 20}}
             full
             rounded
             primary
-            onPress={() => this.SignUpUser(this.state.email, this.state.password)}
-          >
+            onPress={() =>
+              this.SignUpUser(this.state.email, this.state.password)
+            }>
             <Text>Sign Up</Text>
           </Button>
         </Form>
@@ -98,9 +126,13 @@ export default class Open extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
     backgroundColor: '#fff',
+
     //alignItems: 'center',
+
     justifyContent: 'center',
-    padding: 20
+
+    padding: 20,
   },
 });
